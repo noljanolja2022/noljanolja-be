2bdf1aae13d6eb2c2ca231fd6c67114fa0a991ef
Lê Đình Quốc Khánh <149986427+KhanhLDQ@users.noreply.github.com>
2024-02-02 22:43:46 +0700

    plus view count when watching videos (#317)

--- core/src/main/kotlin/com/noljanolja/server/core/rsocket/RSocketController.kt
+++ core/src/main/kotlin/com/noljanolja/server/core/rsocket/RSocketController.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.noljanolja.server.core.exception.Error
 import com.noljanolja.server.core.repo.media.VideoRepo
 import com.noljanolja.server.core.repo.media.toVideoLogTransaction
+import com.noljanolja.server.core.service.VideoService
 import com.noljanolja.server.reward.service.ChatRewardService
 import com.noljanolja.server.reward.service.VideoRewardService
 import kotlinx.coroutines.coroutineScope
@@ -17,13 +18,19 @@ class RSocketController(
     private val chatRewardService: ChatRewardService,
     private val videoRewardService: VideoRewardService,
     private val videoRepo: VideoRepo,
-    private val objectMapper: ObjectMapper
+    private val objectMapper: ObjectMapper,
+    private val videoService: VideoService
 ) {
     @MessageMapping("watch-video")
     suspend fun handleUserWatchVideo(request: UserVideoProgress) {
         with(request) {
             coroutineScope {
                 launch {
+                    //TODO: switch to admin config
+                    if (progressPercentage >= 0.9) {
+                        videoService.viewVideo(videoId)
+                    }
+
                     val video = videoRepo.findById(videoId)
                         ?: throw Error.VideoNotFound
                     val videoLogTransaction = video.toVideoLogTransaction(progressPercentage)
